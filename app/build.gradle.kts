f4e61eb8534731e74119881ed86d8357e737566b
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2023-09-21 01:30:41 +0400

    Requests kinda sorta work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -9,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "cafe.serenity.tbilisiweather"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -47,25 +48,28 @@ android {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:1.6.4")
-    implementation("io.ktor:ktor-client-android:1.6.4")
-    implementation("io.ktor:ktor-client-serialization:1.6.4")
-    implementation("io.ktor:ktor-client-logging:1.6.4")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation( "androidx.core:core-ktx:1.12.0" )
+    implementation( "androidx.appcompat:appcompat:1.6.1" )
+    implementation( "com.google.android.material:material:1.9.0" )
+    implementation( "androidx.compose.ui:ui:1.5.1" )
+    implementation( "androidx.compose.material:material:1.5.1" )
+    implementation( "androidx.compose.ui:ui-tooling-preview:1.5.1" )
+    implementation( "androidx.lifecycle:lifecycle-runtime-ktx:2.6.2" )
+    implementation( "androidx.activity:activity-compose:1.7.2" )
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.5.1" )
+    implementation( "io.ktor:ktor-client-core:1.6.4" )
+    implementation( "io.ktor:ktor-client-android:1.6.4" )
+    implementation( "io.ktor:ktor-client-serialization:1.6.4" )
+    implementation( "io.ktor:ktor-client-logging:1.6.4" )
+    implementation( "ch.qos.logback:logback-classic:1.2.3" )
+
+    implementation( "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0" )
+
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("io.insert-koin:koin-android:3.1.4")
-    implementation("com.google.code.gson:gson:2.8.9")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material3:material3:1.1.1")
 }
--- app/src/main/java/cafe/serenity/tbilisiweather/MainActivity.kt
+++ app/src/main/java/cafe/serenity/tbilisiweather/MainActivity.kt
@@ -1,6 +1,5 @@
 package cafe.serenity.tbilisiweather
 
-
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
@@ -18,14 +17,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.serenity.tbilisiweather.ui.theme.TbilisiWeatherTheme
@@ -42,21 +39,39 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import kotlinx.coroutines.*
 import kotlinx.serialization.json.Json
-import org.koin.androidx.scope.ScopeActivity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val httpClient = HttpClient()
-//
-//        MainScope().launch {
-//            val response = ktorHttpClient.get<ForecastResponse>("https://api.openweathermap.org/data/2.5/weather?lat=41.69411&lon=44.83368&appid=a58c4682a49e63fb623ba14dce0dd7b4")
-//            Log.w("MEOOOOW", "${response}")
-//        }
+        val httpClient = HttpClient()
+
+        MainScope().launch {
+            val response = ktorHttpClient.get<ForecastDTO>("https://api.openweathermap.org/data/2.5/forecast?lat=41.69411&lon=44.83368&appid=a58c4682a49e63fb623ba14dce0dd7b4")
+            setContent {
+                TbilisiWeatherTheme {
+                    val forecast = Forecast()
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color.hsl(190f, 0.5f, 0.7f, 1f)
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                            CurrentWeatherWidget(forecast.currentWeather, Modifier.fillMaxWidth())
+                            TodayWeatherWidget(forecast.todayForecast)
+                            WeaklyWeatherWidget(response.toForecastVM())
+                        }
+                    }
+                }
+            }
+        }
 
         setContent {
             TbilisiWeatherTheme {
@@ -64,14 +79,14 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.hsl(200f, 0.4f, 0.7f, 1f)
+                    color = Color.hsl(190f, 0.5f, 0.7f, 1f)
                 ) {
                     Column(
                         modifier = Modifier.padding(16.dp),
                         verticalArrangement = Arrangement.spacedBy(16.dp)) {
                         CurrentWeatherWidget(forecast.currentWeather, Modifier.fillMaxWidth())
                         TodayWeatherWidget(forecast.todayForecast)
-                        WeaklyWeatherWidget(forecast.weaklyForecast)
+                        WeaklyWeatherWidget(listOf())
                     }
                 }
             }
@@ -80,7 +95,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CurrentWeatherWidget(weather: Weather, modifier: Modifier = Modifier) {
+fun CurrentWeatherWidget(weather: Weather11, modifier: Modifier = Modifier) {
     Column(modifier = Modifier.fillMaxWidth().padding(0.dp, 32.dp, 0.dp, 16.dp)) {
         Text(
             text = "Tbilisi",
@@ -98,15 +113,15 @@ fun CurrentWeatherWidget(weather: Weather, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TodayWeatherWidget(todayWeather: Map<Int, Weather>, modifier: Modifier = Modifier) {
+fun TodayWeatherWidget(todayWeather: Map<Int, Weather11>, modifier: Modifier = Modifier) {
 
     Card(modifier = modifier) {
-        LazyRow(modifier = modifier) {
+        LazyRow(modifier = Modifier.fillMaxWidth()) {
 
             val keys = todayWeather.keys.toIntArray()
             items(keys.size) { itemIdx ->
                 val key = keys[itemIdx]
-                val weather = todayWeather.get(key)
+                val weather = todayWeather[key]
                 Column {
                     Text(
                         text = "${weather?.weatherType?.toEmoji()} ${weather?.temperatureC}°",
@@ -117,7 +132,7 @@ fun TodayWeatherWidget(todayWeather: Map<Int, Weather>, modifier: Modifier = Mod
                         modifier = Modifier.padding(16.dp)
                     )
                 }
-
+                Divider(color = Color.Red)
             }
         }
     }
@@ -125,54 +140,49 @@ fun TodayWeatherWidget(todayWeather: Map<Int, Weather>, modifier: Modifier = Mod
 
 
 @Composable
-fun WeaklyWeatherWidget(weaklyWeather: Map<Int, Weather>, modifier: Modifier = Modifier) {
+fun WeaklyWeatherWidget(forecast: List<WeatherPredicationVM>, modifier: Modifier = Modifier) {
     Card(modifier = modifier) {
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            val keys = weaklyWeather.keys.toIntArray()
-
-            items(keys.size) { itemIdx ->
-
-                val key = keys[itemIdx]
-                val weather = weaklyWeather.get(key)
+            items(forecast.size) { itemIdx ->
+                val weather = forecast[itemIdx]
 
                 Row {
                     Text(
-                        text = "$key",
+                        text = "${weather.date}",
                         modifier = Modifier.padding(16.dp),
                     )
                     Text(
-                        text = "${weather?.weatherType?.toEmoji()} ${weather?.temperatureC}°",
+                        text = "${weather?.weatherType?.toEmoji()} ${weather.temperature.celsius}C°",
                         modifier = Modifier.padding(16.dp),
                     )
                 }
                 Divider(color = Color.White)
-
             }
         }
     }
 }
 
-class Weather(val temperatureC: Int, val weatherType: WeatherType)
+class Weather11(val temperatureC: Int, val weatherType: WeatherType)
 
 class Forecast {
-    val currentWeather = Weather(24, WeatherType.Cloudy)
-    val todayForecast = mapOf<Int, Weather>(
-        0 to Weather(24, WeatherType.Cloudy),
-        2 to Weather(24, WeatherType.Sunny),
-        4 to Weather(24, WeatherType.Cloudy),
-        6 to Weather(24, WeatherType.Rainy),
-        8 to Weather(24, WeatherType.Cloudy),
-        10 to Weather(24, WeatherType.Snowy),
-        12 to Weather(24, WeatherType.Cloudy),
+    val currentWeather = Weather11(24, WeatherType.Cloudy)
+    val todayForecast = mapOf<Int, Weather11>(
+        0 to Weather11(24, WeatherType.Cloudy),
+        2 to Weather11(24, WeatherType.Sunny),
+        4 to Weather11(24, WeatherType.Cloudy),
+        6 to Weather11(24, WeatherType.Rainy),
+        8 to Weather11(24, WeatherType.Cloudy),
+        10 to Weather11(24, WeatherType.Snowy),
+        12 to Weather11(24, WeatherType.Cloudy),
     )
-    val weaklyForecast = mapOf<Int, Weather>(
-        0 to Weather(24, WeatherType.Cloudy),
-        2 to Weather(24, WeatherType.Cloudy),
-        4 to Weather(24, WeatherType.Cloudy),
-        6 to Weather(24, WeatherType.Cloudy),
-        8 to Weather(24, WeatherType.Cloudy),
-        10 to Weather(24, WeatherType.Cloudy),
-        12 to Weather(24, WeatherType.Cloudy),
+    val weaklyForecast = mapOf<Int, Weather11>(
+        0 to Weather11(24, WeatherType.Cloudy),
+        2 to Weather11(24, WeatherType.Cloudy),
+        4 to Weather11(24, WeatherType.Cloudy),
+        6 to Weather11(24, WeatherType.Cloudy),
+        8 to Weather11(24, WeatherType.Cloudy),
+        10 to Weather11(24, WeatherType.Cloudy),
+        12 to Weather11(24, WeatherType.Cloudy),
     )
 }
 
@@ -192,46 +202,131 @@ fun WeatherType.toEmoji(): String {
     }
 }
 
-//private const val TIME_OUT = 60_000
+private const val TIME_OUT = 60_000
 
-//private val ktorHttpClient = HttpClient(Android) {
-//
-//    install(JsonFeature) {
-//        serializer = KotlinxSerializer(Json {
-//            prettyPrint = true
-//            isLenient = true
-//            ignoreUnknownKeys = true
-//        })
-//
-//        engine {
-//            connectTimeout = TIME_OUT
-//            socketTimeout = TIME_OUT
-//        }
-//    }
-//
-//    install(Logging) {
-//        logger = object : Logger {
-//            override fun log(message: String) {
-//                Log.v("Logger Ktor =>", message)
-//            }
-//
-//        }
-//        level = LogLevel.ALL
-//    }
-//
-//    install(ResponseObserver) {
-//        onResponse { response ->
-//            Log.d("HTTP status:", "${response.status.value}")
-//        }
-//    }
-//
-//    install(DefaultRequest) {
-//        header(HttpHeaders.ContentType, ContentType.Application.Json)
-//    }
-//}
+private val ktorHttpClient = HttpClient(Android) {
+
+    install(JsonFeature) {
+        serializer = KotlinxSerializer(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+
+        engine {
+            connectTimeout = TIME_OUT
+            socketTimeout = TIME_OUT
+        }
+    }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.v("Logger Ktor =>", message)
+            }
+
+        }
+        level = LogLevel.ALL
+    }
+
+    install(ResponseObserver) {
+        onResponse { response ->
+            Log.d("HTTP status:", "${response.status.value}")
+        }
+    }
+
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+}
+
+fun ForecastDTO.toForecastVM(): List<WeatherPredicationVM> {
+    return this.data.map {
+        WeatherPredicationVM(
+            date = LocalDateTime.ofInstant(Instant.ofEpochSecond(it.dateTicks), ZoneId.systemDefault()),
+            temperature = Temperature.fromKelvin(it.temperaturePressure.temperatureK),
+            pressureBar = it.temperaturePressure.pressure.toFloat(),
+            weatherType = WeatherType.Sunny,
+            wind = Wind(it.wind.speedMS, it.wind.deg),
+        )
+    }.toList()
+
+}
+
+class Temperature private constructor(private val temperatureK: Float): Comparable<Temperature> {
+    companion object {
+        fun fromKelvin(temperatureK: Float): Temperature {
+            return Temperature(temperatureK)
+        }
+    }
+
+    val kelvin: Float
+        get() {
+            return temperatureK;
+        }
+
+    val celsius: Float
+        get() {
+            return temperatureK - 272.15F
+        }
+    val fahrenheit: Float
+        get() {
+            return 9 * (temperatureK - 273.15f) / 5f + 32f
+        }
+    override fun compareTo(other: Temperature): Int {
+        return temperatureK.compareTo(other.temperatureK)
+    }
+}
+data class WeatherPredicationVM(
+    val date: LocalDateTime,
+    val temperature: Temperature,
+    val pressureBar: Float,
+    val weatherType: WeatherType,
+    val wind: Wind,
+)
+
+data class Wind(val speedMS: Float, val directionDeg: Int)
 
 @Serializable
-data class ForecastResponse(
-    @SerialName("id")
-    val id: String,
+data class ForecastDTO(
+    @SerialName("cod")
+    val code: String,
+    @SerialName("list")
+    val data: List<WeatherDTO>
+)
+
+@Serializable
+data class WeatherDTO( // Have used the name from the API need to figure out smthng better
+    @SerialName("dt")
+    val dateTicks: Long,
+    @SerialName("main")
+    val temperaturePressure: TemperaturePressureDTO,
+    @SerialName("weather")
+    val weatherType: List<WeatherTypeDTO>,
+    @SerialName("wind")
+    val wind: WindDTO,
+)
+
+@Serializable
+data class TemperaturePressureDTO (
+    @SerialName("temp")
+    val temperatureK: Float,
+    @SerialName("pressure")
+    val pressure: Int
+)
+
+@Serializable
+data class WeatherTypeDTO (
+    @SerialName("main")
+    val name: String,
+    @SerialName("description")
+    val description: String
+)
+
+@Serializable
+data class WindDTO(
+    @SerialName("speed")
+    val speedMS: Float,
+    @SerialName("deg")
+    val deg: Int
 )
